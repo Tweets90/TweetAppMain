138b04a4076e3fb6b3e2d688cd091566fd8393f3
Tweets90 <120100353+Tweets90@users.noreply.github.com>
2023-02-16 00:21:21 +0530

    postman api problems resolved

---
+++ src/main/java/com/tweet/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.tweet.repositories;
+
+import com.tweet.entites.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Integer>{
+}
